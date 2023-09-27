2598f8b5cf91a09b3c1e5cf1936dd58755775b4f
Eric <ericluque2003@gmail.com>
2023-09-27 09:19:50 +0200

    About Menu

--- Amarillo Engine/Amarillo Engine/ModuleEditor.cpp
+++ Amarillo Engine/Amarillo Engine/ModuleEditor.cpp
@@ -51,9 +51,40 @@ void ModuleEditor::DrawEditor()
     ImGui::BeginMainMenuBar();
     if (ImGui::BeginMenu("Help"))
     {
-        if (ImGui::MenuItem("Juan"))
+        if (ImGui::BeginMenu("About"))
         {
+            ImGui::Text("Amarillo Engine v0.1");
+            ImGui::BulletText("By Eric Luque & Marcel Sunyer");
+            ImGui::Text("3rd Party Libraries used:");
+            ImGui::BulletText("SDL2");
+            ImGui::BulletText("OpenGL3");
+            ImGui::BulletText("Glew");
+            ImGui::BulletText("MathGeoLib");
+            ImGui::BulletText("ImGui");
+
+            ImGui::Text("MIT License:");
+            ImGui::Text("Copyright (c) 2023 Sergi Perez & Adria Martin");
+            ImGui::Text("Permission is hereby granted, free of charge, to any person obtaining a copy of this software");
+            ImGui::Text("and associated documentation files(the 'Software'), to deal in the Software without ");
+            ImGui::Text("restriction, including without limitation the rights to use, copy, modify, merge, publish, ");
+            ImGui::Text("distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the");
+            ImGui::Text("Software is furnished to do so, subject to the following conditions:");
+            ImGui::Text("The above copyright notice and this permission notice shall be included in all copies or ");
+            ImGui::Text("substantial portions of the Software.");
+            ImGui::Text("THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS");
+            ImGui::Text("OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF");
+            ImGui::Text("MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND");
+            ImGui::Text("NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS");
+            ImGui::Text("BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN");
+            ImGui::Text("ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN");
+            ImGui::Text("CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE");
+            ImGui::Text("SOFTWARE.");
+            ImGui::Separator();
+
+            ImGui::Text("USER GUIDE:");
+            ImGui::ShowUserGuide();
             
+            ImGui::EndMenu();
         }
         ImGui::EndMenu();
     }
