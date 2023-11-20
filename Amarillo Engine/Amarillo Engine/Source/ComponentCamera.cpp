1ba1ae2cf0d05921a06869a2810f1366823c9246
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2023-11-20 21:00:47 +0100

    Component Camera
    aa

--- Amarillo Engine/Amarillo Engine/Source/ComponentCamera.cpp
+++ Amarillo Engine/Amarillo Engine/Source/ComponentCamera.cpp
@@ -24,13 +24,24 @@ void ComponentCamera::Disable()
 
 void ComponentCamera::OnEditor()
 {
-
-
 	if (ImGui::CollapsingHeader("Component Camera"))
 	{
+		float near_plane = camera->GetNearPlaneDistance();
+		if (ImGui::DragFloat("Near plane", &near_plane, 0.2f, 0.01f, 1000))
+		{
+			camera->SetNearPlaneDistance(near_plane);
+		}
 
-		
+		float far_plane = camera->GetFarPlaneDistance();
+		if (ImGui::DragFloat("Far plane", &far_plane, 10, near_plane, 10000))
+		{
+			camera->SetFarPlaneDistance(far_plane);
+		}
 
+		float fov = camera->GetVerticalFOV();
+		if (ImGui::DragFloat("Field of view", &fov, 1, 1, 179.9f))
+		{
+			camera->SetFOV(fov);
+		}
 	}
-
 }
