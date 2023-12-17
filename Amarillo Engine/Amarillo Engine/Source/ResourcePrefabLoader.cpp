8da40b6d21642e5d2923e93885248e1e79f4afd3
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2023-12-17 19:08:07 +0100

    Clear prefab

--- Amarillo Engine/Amarillo Engine/Source/ResourcePrefabLoader.cpp
+++ Amarillo Engine/Amarillo Engine/Source/ResourcePrefabLoader.cpp
@@ -1,6 +1,6 @@
 #include "ResourcePrefabLoader.h"
 
-ResourcePrefabLoader::ResourcePrefabLoader()
+ResourcePrefabLoader::ResourcePrefabLoader() : ResourceLoader(ResourceType::RT_PREFAB, applic->file_system->GetLibraryMeshPath(), "MeshLoader")
 {
 }
 
@@ -11,4 +11,18 @@ ResourcePrefabLoader::~ResourcePrefabLoader()
 bool ResourcePrefabLoader::CreateLibraryFromAsset(std::filesystem::path path, uuids::uuid uid)
 {
 	return false;
+}
+
+Resource* ResourcePrefabLoader::LoadResourceFromLibrary(uuids::uuid guid)
+{
+	return nullptr;
+}
+
+void ResourcePrefabLoader::AddNodeToBinaryStream(const aiScene* scene, aiNode* node, std::vector<char>& stream)
+{
+}
+
+void ResourcePrefabLoader::GetPrefabFromBinaryStream(char* cursor, std::vector<ChildMesh*>& meshes)
+{
+
 }
