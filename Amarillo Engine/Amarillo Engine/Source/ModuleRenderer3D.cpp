7bb18212243640be59218ce7caeb035992712c39
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2024-01-15 13:32:19 +0100

    Tanque subido

--- Amarillo Engine/Amarillo Engine/Source/ModuleRenderer3D.cpp
+++ Amarillo Engine/Amarillo Engine/Source/ModuleRenderer3D.cpp
@@ -185,6 +185,7 @@ bool ModuleRenderer3D::Init()
 	App->scene->LoadMeshAndTexture("../Assets/Models/Skybox.fbx", "../Assets/Textures/skybox.png")->mName = "Skybox";
 	//App->scene->LoadMeshAndTexture("../Assets/Models/BakerHouse.fbx", "../Assets/Textures/Baker_house.png")->mName = "Baker House";
 	App->mesh->LoadMesh("../Assets/Street_Environment/Test.fbx")->mName = "Street_Environment";
+	App->scene->LoadMeshAndTexture("../Assets/Models/Tanque.fbx", "../Assets/Textures/TT_checker_2048x2048_UV_GRID_Base_color.png");
 
 	SDL_MaximizeWindow(App->window->window);
 	BindBuffers();
